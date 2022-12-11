4c8ad0daf4c9aa5f2f1498bb78c01cba3b23ad4f
Paul <pdesyatov@gmail.com>
2022-12-11 14:36:57 +0300

    InsertBeforeAssigned() added.

--- list.cpp
+++ list.cpp
@@ -36,12 +36,12 @@ private:
 
 public:
 
-    Cell* First()
+    Cell* GetFirst()
     {
         return header;
     }
 
-    Cell* Last()
+    Cell* GetLast()
     {
         if (header != nullptr)
         {
@@ -91,7 +91,7 @@ public:
         }
         else//insert to end in non-empty list
         {
-            Cell* oldlast = Last();
+            Cell* oldlast = GetLast();
             oldlast->pos_next = elem;
             elem->pos_next = nullptr;
         }                
@@ -107,7 +107,7 @@ public:
         }
         else//insert to begin in non-empty list
         {
-            Cell* oldfirst = First();
+            Cell* oldfirst = GetFirst();
             elem->pos_next = oldfirst;
             header = elem;     
         }
@@ -130,6 +130,7 @@ public:
                 {
                     //... -> A -> B -> C -> ...
                     // D -> nullptr
+                    // assigned == A
                     // 
                     //... -> A -> D -> B -> C -> ...
                     // 
@@ -148,16 +149,27 @@ public:
 
                 i++;
                 current = current->Next();
+
+                //checking last element:
+                if ((current->Next() == nullptr) && (current == assignedpos))
+                {
+                    InsertToEnd(elem);
+                }
             }
 
             //include last element (equal to situation when assignedpos == nullptr)
+            //in previous cycle While(), last element doesn't participate in cycle (because it points to nullptr)
             if ((current->Next() == nullptr) && (current == assignedpos))
-            {                                   
+            {
+                /*
                 Cell* temp;
 
                 temp = current->Next();//-> nullptr
                 current->pos_next = elem;//A -> D          
-                elem->pos_next = temp; //D -> nullptr                                
+                elem->pos_next = temp; //D -> nullptr
+                */
+
+                InsertToEnd(elem);
             }
             else
             {
@@ -165,6 +177,67 @@ public:
             }
         }
     }    
+
+    void InsertBeforeAssigned(Cell* elem, Cell* assignedpos)//realisation in one-way list (from begin to end)
+    {
+        if (header == nullptr)//try to insert into empty list
+        {
+            std::cout << "Warning! List is empty! inserting failed" << std::endl;
+            //throw std::string("InsertAfterAssigned: list is empty!");
+        }
+        else//insert to non-empty list
+        {
+            Cell* current = header;
+            int i = 0;
+            while (current->Next() != nullptr)
+            {
+                if (current == assignedpos)//equal to inserting to begin
+                {
+                    InsertToBegin(elem);
+                    break;
+                }
+
+                if (current->Next() == assignedpos)
+                {
+                    //... -> A -> B -> C -> ...
+                    // D -> nullptr
+                    // assigned == C
+                    // 
+                    //... -> A -> B -> D -> C -> ...
+                    // 
+                    // temp = -> C
+                    // A->next()->next() = D
+                    // D->next() = C
+
+                    Cell* temp;
+
+                    temp = current->Next();//-> C
+                    current->pos_next = elem;//A -> D
+                    elem->pos_next = temp; //D -> B
+
+                    break;//no need to go further;
+
+                }
+
+                i++;
+                current = current->Next();
+            }
+
+            //include last element (equal to situation when assignedpos == nullptr)
+            if ((current->Next() == nullptr) && (current == assignedpos))
+            {
+                Cell* temp;
+
+                temp = current->Next();//-> nullptr
+                current->pos_next = elem;//A -> D
+                elem->pos_next = temp; //D -> nullptr
+            }
+            else
+            {
+                //throw std::string("InsertAfterAssigned: there is no assigned position in the list!");
+            }
+        }
+    }
 
     void Print()
     {
@@ -195,6 +268,7 @@ public:
     friend std::ostream& operator<< (std::ostream& strm, MyList& a)
     {
         a.Print();
+        std::cout << std::endl;
 
         return strm;
     }
@@ -203,12 +277,33 @@ public:
 
 int main(void)
 {
+    Cell* NewCell_1 = new Cell;
+    NewCell_1->val = 4;
+
+    Cell* NewCell_2 = new Cell;
+    NewCell_2->val = 7;
+
+    Cell* NewCell_3 = new Cell;
+    NewCell_3->val = 9;
+
+    Cell* NewCell_4 = new Cell;
+    NewCell_4->val = 12;
+
+    Cell* NewCell_5 = new Cell;
+    NewCell_5->val = 25;
+
+    Cell* NewCell_6 = new Cell;
+    NewCell_6->val = 66;
+
+
+
+/*
     MyList L1;
 
     Cell* h1;
     Cell* elem;
 
-    h1 = L1.First();
+    h1 = L1.GetFirst();
 
     if (h1 == nullptr)
     {
@@ -222,37 +317,27 @@ int main(void)
 
     Cell* e1;
 
-    e1 = L1.Last();
+    e1 = L1.GetLast();
+*/
 
-//////////////////////////////////////
+///////////////////////////insert to end/////////////////////////////////////////////////
 
-    Cell* NewCell_1 = new Cell;
-    NewCell_1->val = 4;
-
+/*
     L1.InsertToEnd(NewCell_1);
-
-    Cell* NewCell_2 = new Cell;
-    NewCell_2->val = 7;
 
     L1.InsertToEnd(NewCell_2);
 
-    Cell* NewCell_3 = new Cell;
-    NewCell_3->val = 9;
     L1.InsertToEnd(NewCell_3);
 
-    Cell* NewCell_4 = new Cell;
-    NewCell_4->val = 12;
     L1.InsertToEnd(NewCell_4);
-    
-
-    std::cout << std::endl << "   Print test" << std::endl;
-
-    //L1.Print();
 
     std::cout << L1;
 
-//////////////////////////insert to begin//////////////////////////////
+*/
 
+//////////////////////////insert to begin////////////////////////////////////////////////
+
+/*
     MyList L2;
 
     L2.InsertToBegin(NewCell_1);
@@ -264,19 +349,52 @@ int main(void)
     L2.InsertToBegin(NewCell_4);
 
     std::cout << L2;
+*/
 
-////////////////////////////insert after assigned position//////////////////////////////////
+//////////////////////////insert after assigned position/////////////////////////////////
 
+/*
     std::cout << "  insert after assigned position test" << std::endl;
 
-    Cell* NewCell_5 = new Cell;
-    NewCell_5->val = 25;
+    std::cout << L2;
 
-    L2.InsertAfterAssigned(NewCell_5, NewCell_1);//insert after non-edge element
+    //L2.InsertAfterAssigned(NewCell_5, NewCell_1);//insert after non-edge element
     //L2.InsertAfterAssigned(NewCell_5, NewCell_1);//insert after last element
     //L2.InsertAfterAssigned(NewCell_5, NewCell_4);//insert after first element
+    //L2.InsertAfterAssigned(nullptr, NewCell_3);//insert after first element
 
     std::cout << L2;
+
+    MyList EmptyList;
+
+    EmptyList.InsertAfterAssigned(NewCell_5, NewCell_1);
+*/
+
+//////////////////////////insert before assigned position////////////////////////////////
+
+
+    std::cout << "  insert before assigned position test" << std::endl;
+
+    MyList L3;
+
+    L3.InsertToEnd(NewCell_1);//4
+    L3.InsertToEnd(NewCell_2);//7
+    L3.InsertToEnd(NewCell_3);//9
+    L3.InsertToEnd(NewCell_4);//12
+    L3.InsertToEnd(NewCell_5);//25
+
+    std::cout << "L3 : " << std::endl;
+
+    std::cout << L3;
+
+    //L3.InsertBeforeAssigned(NewCell_6, NewCell_3);//insert before non-edge element
+    //L3.InsertBeforeAssigned(NewCell_6, NewCell_5);//insert before last element
+    L3.InsertBeforeAssigned(NewCell_6, NewCell_1);//insert before first element
+
+    std::cout << L3;
+
+
+
 
     return 0;
 }
